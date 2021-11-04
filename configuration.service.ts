9e7d2ecdbdc99ec44452f02a1de026c4849e0fd9
Rickard Nyman <rickard.nyman@castrix.se>
2021-11-04 14:32:43 +0100

    Prototyped Configuration

---
+++ configuration.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigurationService {
+
+  private config: any;
+
+  constructor(private http: HttpClient) { }
+
+  async load() {
+    const data = await this.http.get('/assets/config.json')
+      .toPromise();
+    this.config = data;
+  }
+
+  private isLoaded() {
+    return this.config !== undefined;
+  }
+  
+  public getApiAuthUrl() {
+    if (!this.isLoaded()) {
+      throw Error('Config file not loaded!');
+    }
+    return this.config.apiAuthUrl;
+  }
+
+  public getApiUserUrl() {
+    if (!this.isLoaded()) {
+      throw Error('Config file not loaded!');
+    }
+    return this.config.apiUserUrl;
+  }
+}
